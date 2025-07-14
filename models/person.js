39802256c3af8ca96d9f2b21a7891c96ceb160cf
Lucas <lsterpino@gmail.com>
2025-07-14 11:50:19 -0300

    uibuild

---
+++ models/person.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, minLength: 3, required: true },
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        const vaux = v.split("-");
+        if (vaux.length === 2) {
+          if (vaux[0].length + vaux[1].length >= 8) {
+            if (!isNaN(vaux[0]) && !isNaN(vaux[1])) {
+              if (vaux[0].length === 2 || vaux[0].length === 3) {
+                return true;
+              }
+            }
+          }
+        }
+        return false;
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "Phone number required"],
+  },
+});
+
+personSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.vmvzsnw.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, minLength: 3, required: true },
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((results) => {
+    console.log("phonebook:");
+    results.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  mongoose.connection.close();
+}
